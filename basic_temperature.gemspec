d323f9396ff28b2b945648d9b8e4dac61a928c4c
Marian13 <mariankostyk13895@gmail.com>
2020-03-04 19:00:12 +0200

    T-6 Setup reverse coverage

--- basic_temperature.gemspec
+++ basic_temperature.gemspec
@@ -38,4 +38,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rspec', '~> 3.0'
   spec.add_development_dependency 'rubocop', '~> 0.80.0'
   spec.add_development_dependency 'simplecov'
+  spec.add_development_dependency 'reverse_coverage'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,15 @@
-require_relative 'coverage_helper'
+if ENV['REVERSE_COVERAGE']
+  require_relative 'reverse_coverage_helper'
+else
+  require_relative 'coverage_helper'
+end
 
-require "bundler/setup"
-require "basic_temperature"
+require 'bundler/setup'
+require 'basic_temperature'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
