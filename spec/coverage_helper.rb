30efc5e52d83adc7528a8f1b5adbcb0dc6a70740
Marian13 <mariankostyk13895@gmail.com>
2022-02-11 22:34:37 +0200

    T-79: Set up Coveralls using GitHub Actions

--- spec/coverage_helper.rb
+++ spec/coverage_helper.rb
@@ -1,16 +1,53 @@
-# DOCS: https://github.com/colszowka/simplecov
-# DOCS: https://docs.coveralls.io/ruby-on-rails
+##
+# Coverage using SimpleCov.
+#
+# NOTE: What is SimpleCov?
+# https://github.com/simplecov-ruby/simplecov
+#
+# NOTE: What is lcov and how to set up it?
+# - https://github.com/fortissimo1997/simplecov-lcov
+# - https://github.com/tagliala/coveralls-ruby-reborn#github-actions
+#
+# NOTE: How to deploy coverage data to Coveralls using GitHub Actions?
+# - https://github.com/marketplace/actions/coveralls-github-action
+# - https://github.com/coverallsapp/github-action/issues/29
+#
+# IMPORTANT: Why `basic_temperature/version.rb` is ignored? See:
+# https://github.com/simplecov-ruby/simplecov/issues/557
+#
+
+##
+# HACK: Fixes the following error for older Ruby versions.
+#
+#   Formatter SimpleCov::Formatter::LcovFormatter failed with NoMethodError: undefined method `branch_coverage?' for SimpleCov:Module
+#
+# https://github.com/fortissimo1997/simplecov-lcov/pull/25/files
+#
+# TODO: Remove when support for Rubies lower than 2.5 will be dropped.
+#
+if Gem::Version.new(RUBY_VERSION) < Gem::Version.new('2.5')
+  module SimpleCov
+    class << self
+      def branch_coverage?
+        false
+      end
+    end
+  end
+end
+
 require 'simplecov'
-require 'coveralls'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config do |config|
+  config.report_with_single_file = true
+  config.single_report_path = 'coverage/lcov.info'
+end
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
+  SimpleCov::Formatter::LcovFormatter
 ])
 
 SimpleCov.start do
   add_filter '/spec/'
 end
-
-# NOTE: `basic_temperature/version.rb` is ignored intentionally.
-# See https://github.com/colszowka/simplecov/issues/557
